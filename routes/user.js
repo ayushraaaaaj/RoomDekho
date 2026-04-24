ccaf4f05d8ac8af35aed742219aefd09e230706b
ayushraaaaaj <workwithayush2024@gmail.com>
2026-04-24 23:52:55 +0530

    Login started

---
+++ routes/user.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+    res.render("users/signup.ejs");
+});
+
+router.post("/signup", wrapAsync(async(req, res) => {
+    try {
+        let {username, email, password} = req.body;
+        const newUser = new User({email, username});
+        const registeredUser = await User.register(newUser, password);
+        req.flash("success", "User was registered successfully");
+        res.redirect("/listings");
+    } catch(e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+}));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), async (req, res) => {
+    req.flash("success", "Welcome back to room_dekho");
+    res.redirect("/listings");
+});
+
+module.exports = router;
